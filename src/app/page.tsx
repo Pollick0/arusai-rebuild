4adffd6f5e1951686b352cbce8557eae38ddf6b6
44David <david.soane@outlook.com>
2025-02-21 12:29:49 -0600

    Created first version of hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,29 @@
 export default function Home() {
   return (
     <main>
-      
+      <div className="w-screen p-48 border border-gray-400 mx-auto text-center">
+
+        <h1 className="font-bold text-5xl">
+          Streamline your advertising
+          <br></br>
+          with our website solutions 
+        </h1>  
+
+        <h2>
+          Just a few clicks away.
+        </h2>
+
+        <button className="bg-red-950 p-2 px-8 my-2 rounded-md shadow-2xl shadow-red-800/50 font-bold text-md hover:shadow-red-900/50 hover:shadow-xl hover:bg-red-900">
+          Start the journey 
+        </button>
+
+
+
+      </div>
+
+      <div>
+
+      </div>
     </main>
   );
 }
